76e01286b40944d1932cca5c180fd3b79a759d05
zacharylondono <zcl6@butter.cs.rutgers.edu>
2020-04-21 17:10:31 -0400

    accepts multiple connections and creates a thread for each

--- server.c
+++ server.c
@@ -11,6 +11,19 @@
 #include "sharedfunctions.h"
 #include "servercommands.h"
 
+void* threadentry(void* value) {
+
+	int clisockfd = *(int*)value;
+
+	NetworkCommand* message =  readMessage(clisockfd);
+
+	printf("LOG: Message recieved from client -> %d\n", message->type);
+	executecommand(message, clisockfd);
+	freeCMND(message);
+
+	return NULL;
+}
+
 int main(int argc, char** argv) {
 
 	if (argc != 2) {
@@ -48,72 +61,28 @@ int main(int argc, char** argv) {
 	struct sockaddr_in cli_addr;
 	socklen_t clilen = sizeof(cli_addr);
 
-	int clisockfd = accept(serverfd, (struct sockaddr*)&cli_addr, &clilen);
+	int clisockfd = 0;
+	while (1) {
+		
+		clisockfd = accept(serverfd, (struct sockaddr*)&cli_addr, &clilen);
+		
+		if (clisockfd < 0) {
+			printf("Error: couldn't open client socket\n");
+			continue;
+		}
 
-	if (clisockfd < 0) {
-		printf("Error: couldn't open client socket\n");
-		return -1;
+		printf("Successful connection from: %s\n", inet_ntoa((&cli_addr)->sin_addr));
+
+		pthread_t thread;
+		pthread_create(&thread, NULL, threadentry, &clisockfd);
+		pthread_join(thread, NULL);
+
 	}
 
-	printf("Successful connection from: %s\n", inet_ntoa((&cli_addr)->sin_addr));
+	printf("An error occured while accepting new connections: %s\n", strerror(errno));
 
-	NetworkCommand* message =  readMessage(clisockfd);
-	
-	printf("LOG: Message recieved from client -> %d\n", message->type);
-	executecommand(message, clisockfd);
-	freeCMND(message);
-
-	// int serverfd = socket(AF_INET, SOCK_STREAM,0);
-
-	// if (serverfd < 0) {
-	// 	printf("Error: server failed to open socket\n");
-	// 	return -1;
-	// }
-
-	// if (bindtoport(serverfd, port) < 0) {
-	// 	printf("Error: failed to bind socket to port\n");
-	// 	return -1;
-	// }
-	
-	// if (listen(serverfd, 5) < 0) {
-	// 	printf("Error: unable to listen on port\n%s\n", strerror(errno));
-	// }
-
-
-	// struct sockaddr_in cli_addr;
-	// int clilen = sizeof(cli_addr);
-	// int newsockfd = accept(serverfd, (struct sockaddr *)&cli_addr, &clilen);
-	// if (newsockfd < 0) {
-	// 	printf("Error: couldn't accept client connection\n%s\n", strerror(errno));
-	// 	return -1;
-	// } else {
-	// 	printf("Client successfully connected: \n");
-	// 	printf(": %s\n", inet_ntoa((&cli_addr)->sin_addr));
-	// }
-
-	// char buffer[256];
-	// bzero(buffer, 256);
-	
-	// int n = read(newsockfd, buffer, 255);
-
-	// if (n < 0) {
-	// 	perror("error reading");
-	// 	return 0;
-	// }
-
-	// printf("Message from client: %s\n", buffer);
-
-	// n = write(newsockfd, "message recieved", 18);
-
-	// if (n < 0) {
-	// 	perror("Error writing");
-	// 	return 0;
-	// }
+	// TODO: close all threads
 
 	return 0;
 
 }
-
-// int executeServerCommand(NetworkCommand command) {
-
-// }
