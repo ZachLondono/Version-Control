1d878faee019178469d7c0b91302efe6c55def7d
zacharylondono <60956358+zacharylondono@users.noreply.github.com>
2020-04-22 12:06:01 -0400

    removed checknet

--- servercommands.h
+++ servercommands.h
@@ -8,7 +8,6 @@ FileContents* serverreadfile(char* name);
 ssize_t serverwrite(int fd, char* buff, int count);
 
 int _responsenet(NetworkCommand* command, int sockfd);
-int _checknet(NetworkCommand* command, int sockfd);
 int _createnet(NetworkCommand* command, int sockfd);
 int _destroynet(NetworkCommand* command, int sockfd);
 int _projectnet(NetworkCommand* command, int sockfd);
